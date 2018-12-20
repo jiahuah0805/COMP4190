4da0d5c56aefa71e266e0f4be12464da1e9cf91c
jiahuah0805 <31642780+jiahuah0805@users.noreply.github.com>
2018-12-20 02:44:36 -0800

    Add files via upload
    Project draft

---
+++ COMP 4160 Project/HTML/JS lib/index.js
@@ -0,0 +1,50 @@
+
+var ccount = document.getElementById("ccount");
+var btns = document.querySelectorAll(".list dl dd button");
+
+
+var listStr = cookieObj.get("datas");
+
+if(!listStr) {
+	cookieObj.set({
+		name: "datas",
+		value: "[]"
+	});
+	listStr = cookieObj.get("datas");
+}
+
+var listObj = JSON.parse(listStr);
+
+var totalCount = 0;
+for(var i = 0, len = listObj.length; i < len; i++) {
+	totalCount = listObj[i].pCount + totalCount;
+}
+ccount.innerHTML = totalCount;
+
+
+for(var i = 0, len = btns.length; i < len; i++) {
+	btns[i].onclick = function() {
+		var dl = this.parentNode.parentNode;
+		var pid = dl.getAttribute("pid");
+		var arrs = dl.children;
+		if(checkObjByPid(pid)) {
+			listObj = updateObjById(pid, 1)
+		} else {
+			var imgSrc = arrs[0].firstElementChild.src;
+			var pName = arrs[1].innerHTML;
+			var pDesc = arrs[2].innerHTML;
+			var price = arrs[3].firstElementChild.innerHTML;
+			var obj = {
+				pid: pid,
+				pImg: imgSrc,
+				pName: pName,
+				pDesc: pDesc,
+				price: price,
+				pCount: 1
+			};
+			listObj.push(obj);
+			listObj = updateData(listObj);
+		}
+		ccount.innerHTML = getTotalCount();
+	}
+}
